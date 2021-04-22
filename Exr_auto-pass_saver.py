8c983f905776174fe761baec5b306504b0d5133f
squindt <sergej.batgauer@gmail.com>
2021-04-22 15:54:14 +0200

    Update Exr_auto-pass_saver.py
    fix: open destination folder not working, if path is relative
    hardcoded color_mode to rgba, color_depth to half-float, exr_codec to "zips" and filepath to "//render/"
    feature: option to set main output format to JPEG to use less storage when rendering, because this main output can not be deactivated and is useless when getting all passes from this addon
    feature: option for outputting single EXR per pass, MultiEXRs can be outputted by default from the main output.
    feature: if OIDN denoising data pass is activated, denoised image pass is added (denoise node between layer and output) instead of "denoising normal", "denoising albedo" etc., seems more useful, if compositing in other software than blender
    feature: if "use nodes" is not activated in blender compositor, it gets activated automatically

--- Exr_auto-pass_saver.py
+++ Exr_auto-pass_saver.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "Exr Auto Pass Saver",
-    "description": "Link all render passes to a new EXR-MulitLayer save node",
+    "description": "Link all render passes to a new EXR-MulitLayer or EXR-SingleLayer save node",
     "blender": (2, 80, 0),
     "category": "Compositing",
     "author": "3d-io",
@@ -17,21 +17,34 @@ import os
 
 bpy.types.Scene.exr_auto_pass_saver_clear_all = bpy.props.BoolProperty(
     name="Clear all nodes",
-        description="Remove all nodes from the Compositor and add only RenderLayer <-> Saver Node",
-        default = False)
+    description="Remove all nodes from the Compositor and add only RenderLayer <-> Saver Node",
+    default=False,
+)
 
 
 bpy.types.Scene.exr_auto_pass_saver_open_dir = bpy.props.BoolProperty(
     name="Open destination folder",
-        description="A folder where the Exr Image is going to be saved",
-        default = False)
+    description="A folder where the Exr Image is going to be saved",
+    default=False,
+)
+
+bpy.types.Scene.single_files = bpy.props.BoolProperty(
+    name="Single EXRs",
+    description="Create a file output node for each render pass",
+    default=True,
+)
+
+bpy.types.Scene.main_out_to_jpeg = bpy.props.BoolProperty(
+    name="Set main to JPEG",
+    description="Set main output format to JPEG to reduce storage usage",
+    default=True,
+)
 
 
 class Exr_Auto_Pass_Saver(bpy.types.Operator):
     bl_idname = "node.exr_pass_saver"
-    bl_label = "Exr Auto Saver"
-    bl_options = {'REGISTER', 'UNDO'}
-
+    bl_label = "EXR Auto Saver"
+    bl_options = {"REGISTER", "UNDO"}
 
     # removes all currently existing nodes
     def cleannodes(self):
@@ -39,38 +52,63 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
         for currentNode in nodesField.nodes:
             nodesField.nodes.remove(currentNode)
 
-
     # opens the directory of the current render path
     def openfolder(self):
-        path = bpy.data.scenes["Scene"].render.filepath
-        subprocess.call("explorer " + path, shell=True)
-
+        path = bpy.path.abspath(bpy.context.scene.render.filepath)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        subprocess.call("explorer " + path.strip("/"), shell=True)
 
     # returns a target render path taken from the scene's render output and cleaned up as needed
     def GetOutputPathStr(self):
-        defaultPath = bpy.context.scene.render.filepath
-        dirname = os.path.dirname(defaultPath)
-        if not dirname.endswith( ('/', '\\') ):
-            dirname += "/"
-        if not dirname:
-            dirname = os.path.expanduser('~') + "/"
-        fileName = "output"
-        fullPath = dirname + fileName
+        if bpy.context.scene.single_files:
+            fullPath = os.path.join(
+                bpy.path.abspath(bpy.context.scene.render.filepath), "SingleEXRs"
+            )
+        else:
+            fullPath = os.path.join(
+                bpy.path.abspath(bpy.context.scene.render.filepath),
+                "MultiEXRs",
+                "renderpasses.",
+            )
         return fullPath
-
 
     # creates a new Render Layers node at the given position
     def CreateNodeRenderLayers(self, position):
-        node = bpy.context.scene.node_tree.nodes.new('CompositorNodeRLayers')
+        node = bpy.context.scene.node_tree.nodes.new("CompositorNodeRLayers")
         node.location = position
         return node
 
-
     # creates a new Output File node at the given position
-    def CreateNodeFileOutput(self, position):
-        bpy.context.scene.render.image_settings.file_format = 'OPEN_EXR_MULTILAYER'
+    def CreateNodeFileOutput(self, position, sourceNode):
+        scene = bpy.context.scene
+
+        if not scene.single_files:
+
+            bpy.context.scene.render.image_settings.file_format = "OPEN_EXR_MULTILAYER"
+            bpy.context.scene.render.image_settings.color_mode = "RGBA"
+            bpy.context.scene.render.image_settings.color_depth = "16"
+            bpy.context.scene.render.image_settings.exr_codec = "ZIPS"
+            bpy.context.scene.render.filepath = "//render/"
+
+            node = bpy.context.scene.node_tree.nodes.new("CompositorNodeOutputFile")
+            node.label = "EXR-MultiLayer"
+            node.base_path = self.GetOutputPathStr()
+            node.location = position
+            node.width = 300
+            node.width_hidden = 300
+            node.use_custom_color = True
+            node.color = (0.686, 0.204, 0.176)
+            return node
+
+        bpy.context.scene.render.image_settings.file_format = "OPEN_EXR"
+        bpy.context.scene.render.image_settings.color_mode = "RGBA"
+        bpy.context.scene.render.image_settings.color_depth = "16"
+        bpy.context.scene.render.image_settings.exr_codec = "ZIPS"
+        bpy.context.scene.render.filepath = "//render/"
+
         node = bpy.context.scene.node_tree.nodes.new("CompositorNodeOutputFile")
-        node.label = 'EXR-MultiLayer'
+        node.label = "EXRs"
         node.base_path = self.GetOutputPathStr()
         node.location = position
         node.width = 300
@@ -79,17 +117,43 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
         node.color = (0.686, 0.204, 0.176)
         return node
 
-
     # links all outputs of the source node to inputs of the target node
     def LinkRenderLayers(self, sourceNode, targetNode):
         for out in sourceNode.outputs:
+
             # skip disabled outputs
-            if (out.enabled == False):
+            if out.enabled == False:
                 continue
+
+            # Combine OIDN Denoising Data to a single denoised pass
+            if out.identifier.startswith("Denoising"):
+                denoise_node = bpy.context.scene.node_tree.nodes.new(
+                    "CompositorNodeDenoise"
+                )
+                denoise_node.location = (450, 200)
+                bpy.context.scene.node_tree.links.new(
+                    sourceNode.outputs["Image"], denoise_node.inputs["Image"]
+                )
+                bpy.context.scene.node_tree.links.new(
+                    sourceNode.outputs["Denoising Normal"],
+                    denoise_node.inputs["Normal"],
+                )
+                bpy.context.scene.node_tree.links.new(
+                    sourceNode.outputs["Denoising Albedo"],
+                    denoise_node.inputs["Albedo"],
+                )
+                targetNode.file_slots.new("Denoised_Image.")
+                bpy.context.scene.node_tree.links.new(
+                    denoise_node.outputs["Image"],
+                    targetNode.inputs["Denoised_Image."],
+                )
+                bpy.context.scene.render.image_settings.file_format
+                return
+
             slot = 0
             found = False
             for src in targetNode.inputs:
-                if (src.identifier == out.identifier):
+                if src.identifier == out.identifier:
                     # target node already has matching input, link to it
                     found = True
                     bpy.context.scene.node_tree.links.new(out, targetNode.inputs[slot])
@@ -97,18 +161,16 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
                 slot = slot + 1
             if not found:
                 # target node has no matching input, create one and link to it
-                targetNode.file_slots.new(out.identifier)
+                targetNode.file_slots.new(out.identifier.replace(" ", "_") + ".")
                 bpy.context.scene.node_tree.links.new(out, targetNode.inputs[-1])
-
 
     # Exr Auto Saver button
     # Generates reqired nodes for the OpenEXR output and links available render passes
     def execute(self, context):
-        scene = bpy.context.scene # get the scene
+        scene = bpy.context.scene  # get the scene
 
         if not scene.use_nodes:
-            print('Info: node_tree not in use. Skipping')
-            return {'CANCELLED'}
+            bpy.context.scene.use_nodes = 1
 
         if scene.exr_auto_pass_saver_clear_all:
             self.cleannodes()
@@ -117,10 +179,11 @@ class Exr_Auto_Pass_Saver(bpy.types.Operator):
             self.openfolder()
 
         layersNode = self.CreateNodeRenderLayers((0, 400))
-        outputNode = self.CreateNodeFileOutput((400, 450))
+        outputNode = self.CreateNodeFileOutput((800, 450), layersNode)
         self.LinkRenderLayers(layersNode, outputNode)
-        return {'FINISHED'}
-
+        if scene.main_out_to_jpeg:
+            bpy.context.scene.render.image_settings.file_format = "JPEG"
+        return {"FINISHED"}
 
 
 class Exr_Auto_Pass_Saver_Panel(bpy.types.Panel):
@@ -135,11 +198,13 @@ class Exr_Auto_Pass_Saver_Panel(bpy.types.Panel):
         # draw Auto Saver button
         row = layout.row()
         row.scale_y = 2.0
-        row.operator(Exr_Auto_Pass_Saver.bl_idname, icon='TRACKING_FORWARDS')
+        row.operator(Exr_Auto_Pass_Saver.bl_idname, icon="TRACKING_FORWARDS")
         sce = context.scene
         # draw the checkbox (implied from property type = bool)
-        layout.prop(sce, "exr_auto_pass_saver_clear_all") 
-        layout.prop(sce, "exr_auto_pass_saver_open_dir") 
+        layout.prop(sce, "exr_auto_pass_saver_clear_all")
+        layout.prop(sce, "exr_auto_pass_saver_open_dir")
+        layout.prop(sce, "single_files")
+        layout.prop(sce, "main_out_to_jpeg")
 
 
 classes = (Exr_Auto_Pass_Saver_Panel, Exr_Auto_Pass_Saver)
